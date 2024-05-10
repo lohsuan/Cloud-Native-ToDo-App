370272e8959334c3950bd86d1670ecccbd576210
erinlo <angelalo88362@gmail.com>
2024-05-10 15:53:09 +0800

    change frontend title to This is My Todos List

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -44,7 +44,7 @@ const App = () => {
 
   return (
     <main className="App">
-      <h1>My Todos List</h1>
+      <h1>This is My Todos List</h1>
       <AddTodo saveTodo={handleSaveTodo} />
       {todos.map((todo: ITodo) => (
         <TodoItem key={todo.id} updateTodo={handleUpdateTodo} deleteTodo={handleDeleteTodo} todo={todo} />
