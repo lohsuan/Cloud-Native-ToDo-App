47168b241985715cd15452bca240f1820b4ead69
Yu-Hsuan, Lo (Erin Lo) <52515354+lohsuan@users.noreply.github.com>
2024-05-17 14:14:42 +0800

    Revert "Revert "Feature add ci cd"" (#3)

--- backend/test/todo.spec.ts
+++ backend/test/todo.spec.ts
@@ -23,4 +23,59 @@ describe('Todo API Testing', () => {
     const todos = JSON.parse(response.body)['todos']
     expect(todos).toStrictEqual([])
   })
+
+  test('Given a todo object return from repo function, When send a POST request to /api/v1/todos, Then it should response the todo object', async () => {
+    // arrange: a todo object
+    const todo: Todo = {
+      id: '1',
+      name: 'mock todo',
+      description: 'mock description',
+      status: false
+    }
+
+    // assert: stub the repo function to return the todo object
+    vi.spyOn(TodoRepo, 'createTodo').mockImplementation(async () => todo)
+
+    // act: send a POST request to /api/v1/todos
+    const response = await server.inject({
+      method: 'POST',
+      url: '/api/v1/todos',
+      payload: {
+        id: '1',
+        name: 'mock todo',
+        description: 'mock description',
+        status: false
+      }
+    })
+
+    // assert: response should be the todo object
+    const newTodo = JSON.parse(response.body)['todo']
+    expect(newTodo).toStrictEqual(todo)
+  })
+
+  test('Given a todo object return from repo function, When send a PUT request to /api/v1/todos/:id, Then it should response the todo object', async () => {
+    // arrange: a todo object
+    const todo: Todo = {
+      id: '1',
+      name: 'mock todo',
+      description: 'mock description',
+      status: true
+    }
+
+    // assert: stub the repo function to return the todo object
+    vi.spyOn(TodoRepo, 'updateTodoById').mockImplementation(async () => todo)
+
+    // act: send a PUT request to /api/v1/todos/:id
+    const response = await server.inject({
+      method: 'PUT',
+      url: '/api/v1/todos/1',
+      payload: {
+        status: true
+      }
+    })
+
+    // assert: response should be the todo object
+    const updatedTodo = JSON.parse(response.body)['todo']
+    expect(updatedTodo).toStrictEqual(todo)
+  })
 })
